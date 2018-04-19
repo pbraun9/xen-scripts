f7b71ce62dd279d954fc950cbca7081239ea78fd
pierre-philipp <pbraun@nethence.com>
2018-04-19 15:35:17 +0300

    _

--- slackware.install.bash
+++ slackware.install.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 
+repo=/data/media/slackware/142
 slackmount=/tmp/slack
 dummy=0
 
 for set in a ap d f l n; do
-	for pkg in `egrep ':ADD$|:REC$' slackware64/$set/tagfile | cut -f1 -d:`; do
-	        #egrep "^slackware64/$set/$pkg-[[:alnum:]\._]+-[[:alnum:]_]+-[[:digit:]]+.txz$"
+	for pkg in `grep :ADD$ $repo/$set/tagfile | cut -f1 -d:`; do
+	        #egrep "^$repo/$set/$pkg-[[:alnum:]\._]+-[[:alnum:]_]+-[[:digit:]]+.txz$"
 		pkgfix=`echo $pkg | sed 's/+/\\\+/g'`
-	        pkgfile=`find slackware64/ -type f | egrep "^slackware64/$set/$pkgfix-[^-]+-[^-]+-[^-]+.txz$"`
+	        pkgfile=`find $repo/ -type f | egrep "^$repo/$set/$pkgfix-[^-]+-[^-]+-[^-]+.txz$"`
 	        [[ -z $pkgfile ]] && echo no txz archive found for $pkg && exit 1
 		(( `echo $pkgfile | wc -l` != 1 )) && echo "too much results for $pkg:\n$pkgfile" && exit 1
 		echo -n installpkg --root $slackmount $pkgfile...
