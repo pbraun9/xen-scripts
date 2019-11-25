6a4f14432c71e5accbd76c0a8aa4e7f9f00752db
elge@oldschool <pbraun@nethence.com>
2019-11-25 11:23:30 +0300

    import from oldschool

--- running.guests.bash
+++ running.guests.bash
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-echo RUNNING GUESTS
-dsh -e -g xen "xl li | sed '1,2d' | awk '{print $1}'"
-echo ''
-
+#dsh -e -g xen "xl li | sed '1,2d' | awk '{print $1}'"
+dsh -e -g xen "xl li | sed '1,2d'"
--- running.hosts.bash
+++ running.hosts.bash
@@ -10,6 +10,12 @@ echo ''
 
 echo LINUX VERSIONS
 dsh -e -g cluster "uname -r"
+dsh -e -g cluster "file /vmlinuz"
+dsh -e -g cluster "ls -lhF /lib/modules/"
+echo ''
+
+echo DOM0 KERNEL ARG
+dsh -e -g xen "grep xen.gz /extlinux.conf"
 echo ''
 
 echo FILE-SYSTEMS
@@ -21,11 +27,52 @@ echo MEMORY \(MB\)
 dsh -e -g cluster "free -m | grep ^Mem"
 echo ''
 
-echo INTERNET \(ping 208.67.220.220\)
+#echo DOM0 WEIGHT BOOT ARG
+#dsh -e -g xen "grep sched-credit /etc/rc.d/rc.local"
+#echo ''
+
+echo DOM0 WEIGHT
+dsh -e -g xen "xl sched-credit | grep Domain-0"
+echo ''
+
+echo DOM0 BALLOON
+dsh -e -g xen "grep balloon /etc/xen/xl.conf"
+echo ''
+
+#echo NETWORK LAYER 1
+#dsh -e -g cluster -s running.hosts.bash.netif
+#echo ''
+
+echo NETWORK AGGREGATE
+dsh -e -g cluster "cat /sys/class/net/bonding_masters /sys/class/net/bond0/speed"
+dsh -e -g cluster "cat /proc/net/bonding/bond0 | egrep '^Speed|^Bonding Mode'"
+echo ''
+
+echo JUMBO FRAMES
+dsh -e -g cluster "for netif in eth1 pubbr0 eth2 eth3 bond0 xenbr0; do echo -n \$netif: ; ifconfig \$netif | head -1 | awk '{print \$NF}'; done; unset netif"
+echo ''
+
+echo QUEUE LENGTH
+dsh -e -g cluster "for netif in eth1 pubbr0 eth0 eth2 eth3 bond0 xenbr0; do echo -n \$netif: ; ifconfig \$netif | grep txqueuelen | awk '{print \$4}'; done; unset netif"
+echo ''
+
+echo GRO
+dsh -e -g cluster "for nic in eth1 eth0 eth2 eth3; do echo -n \$netif: ; ethtool -k \$nic 2>/dev/null | grep gro; done; unset nic"
+echo ''
+
+echo BNX2FIX
+dsh -e -g cluster "cat /etc/modprobe.d/bnx2.conf"
+dsh -e -g cluster "cat /sys/module/bnx2/parameters/disable_msi"
+echo ''
+
+echo PUBLIC NETWORK
 dsh -e -g cluster "ping -c1 -W1 208.67.220.220 >/dev/null && echo OK || echo NOK"
 echo''
 
-echo INTERNET \(ping opendns\)
+echo PUBLIC NAME RESOLUTION
 dsh -e -g cluster "ping -c1 -W1 opendns.com >/dev/null && echo OK || echo NOK"
 echo ''
+
+echo PACKAGES
+dsh -e -g cluster "ls -1 /var/log/packages/ | grep tigervnc"
 
