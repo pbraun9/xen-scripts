85174202fd1e23b4afa5b58088e8057395082da3
root@slack1 <root@slack1>
2021-10-30 17:51:29 +0300

    state check

--- shuguest.bash
+++ shuguest.bash
@@ -3,6 +3,7 @@ echo
 
 [[ -z $1 ]] && echo -e ${0##*/} GUEST-NAME\\n && exit 1
 guest=$1
+guestpath=/data/guests/$guest
 
 node=`dsh -e -g xen "xl list | grep \"^$guest \"" | cut -f1 -d:`
 
@@ -12,5 +13,6 @@ node=`dsh -e -g xen "xl list | grep \"^$guest \"" | cut -f1 -d:`
 	echo ERROR guest lives on multiple nodes! &&\
 	echo "$liveson" && exit 1
 
+ssh $node echo down > $guestpath/state
 ssh $node xl shu $guest && echo -e \\nSHUTTING DOWN GUEST $guest ON NODE $node\\n
 
