3f28c66bac293d53715a327c729a5f3cb781588f
root@slack1 <root@slack1>
2021-11-10 19:33:52 +0200

    graceful shutdown and keep track of guest states

--- runguest.bash
+++ runguest.bash
@@ -1,8 +1,11 @@
 #!/bin/bash
-echo
+set -e
 
-[[ -z $1 ]] && echo -e ${0##*/} GUEST-NAME\\n && exit 1
+[[ -z $1 ]] && echo ${0##*/} GUEST-NAME && exit 1
 guest=$1
+
+node=`dsh -e -g xen "xl list | grep -E \"^$guest[[:space:]]+\"" | cut -f1 -d:`
+[[ -n $node ]] && echo $guest already lives on $node exit 1
 
 (( nodeid = RANDOM % 3 + 1 ))
 node=slack${nodeid}
@@ -11,6 +14,6 @@ guestpath=/data/guests/$guest
 conf=$guestpath/$guest
 [[ ! -f $conf ]] && echo $conf cannot be found && exit 1
 
-ssh $node echo up > $guestpath/state
-ssh $node xl create $conf && echo -e \\nGUEST $guest HAS BEEN STARTED ON NODE $node\\n
+ssh $node "xl create $conf" && echo GUEST $guest HAS BEEN STARTED ON NODE $node
+ssh $node "echo up > $guestpath/state"
 
--- shuguest.bash
+++ shuguest.bash
@@ -1,18 +1,29 @@
 #!/bin/bash
-echo
 
 [[ -z $1 ]] && echo -e ${0##*/} GUEST-NAME\\n && exit 1
 guest=$1
 guestpath=/data/guests/$guest
 
-node=`dsh -e -g xen "xl list | grep \"^$guest \"" | cut -f1 -d:`
+node=`dsh -e -g xen "xl list | grep -E \"^$guest[[:space:]]+\"" | cut -f1 -d:`
+[[ -z $node ]] && echo $guest does not seem to be alive && exit 1
+(( `echo "$node" | wc -l` > 1 )) && echo -e "ERROR guest lives on multiple nodes!\n$node" && exit 1
 
-[[ -z $node ]] && echo guest $guest does not seem to be alive && exit 1
+ssh $node "xl shu $guest" && echo -n SHUTTING DOWN GUEST $guest ON NODE $node
 
-(( `echo "$node" | wc -l` > 1 )) &&\
-	echo ERROR guest lives on multiple nodes! &&\
-	echo "$liveson" && exit 1
+(( maxsec = 25 ))
+(( sec = 0 ))
+until (( sec > maxsec )); do
+	sleep 1
+	tmp=`ssh $node "xl list | grep -E \"^$guest[[:space:]]+\"" | cut -f1 -d:`
+	[[ -n $tmp ]] && echo -n .
+	[[ -z $tmp ]] && echo DONE && break
+	(( sec++ ))
+	unset tmp
+done
 
-ssh $node echo down > $guestpath/state
-ssh $node xl shu $guest && echo -e \\nSHUTTING DOWN GUEST $guest ON NODE $node\\n
+tmp=`ssh $node "xl list | grep -E \"^$guest[[:space:]]+\"" | cut -f1 -d:`
+[[ -n $tmp ]] && echo $guest failed to shut down gracefully && ssh $node xl destroy $guest
+unset tmp
+
+ssh $node "echo down > $guestpath/state"
 
