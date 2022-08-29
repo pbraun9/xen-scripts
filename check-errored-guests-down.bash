76d17664dec1a35b6216b91eb2d7fcca6162d029
root@pmr2 <root@pmr2>
2022-08-29 11:38:55 +0300

    show only missings guests that should be up

--- check-errored-guests-down.bash
+++ check-errored-guests-down.bash
@@ -3,5 +3,5 @@
 should=`grep ^up$ /data/guests/*/state | cut -f4 -d/ | sort -V`
 up=`dsh -e -g xen "xl list | sed '1,2d' | cut -f1 -d' '" | awk '{print $2}' | sort -V`
 
-diff -bu <(echo "$should") <(echo "$up") | grep ^- | sed 's/^-//'
+diff -bu <(echo "$should") <(echo "$up") | grep ^+ | sed 's/^+//'
 
